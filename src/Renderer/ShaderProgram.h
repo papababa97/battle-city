415a62a7c38296c65937ec7d7b7fd133e3f24df6
Oleg <olegzaripov80@gmail.com>
2023-02-10 15:27:40 +0500

    add ShaderProgram Class

---
+++ src/Renderer/ShaderProgram.h
@@ -0,0 +1,26 @@
+#ifndef SHAREDPROGRAM_H__
+#define SHAREDPROGRAM_H__
+
+#include <string>
+#include <glad/glad.h>
+
+namespace Renderer {
+	class ShaderProgram {
+	public:
+		ShaderProgram(const std::string &vertexShader, const std::string &fragmentShader);
+		~ShaderProgram();
+		bool isCompiled() const { return m_isCompiled; }
+		void use() const;
+
+		ShaderProgram() = delete;
+		ShaderProgram(ShaderProgram&) = delete;
+		ShaderProgram& operator=(const ShaderProgram&) = delete;
+		ShaderProgram& operator=(ShaderProgram&& ShaderProgram) noexcept;
+		ShaderProgram(ShaderProgram&& ShaderProgram) noexcept;
+	private:
+		bool createShader(const std::string &source, const GLenum shaderType, GLuint& shaderID);
+		bool m_isCompiled = false;
+		GLuint m_ID = 0;
+	};
+}
+#endif //SHAREDPROGRAM_H__
